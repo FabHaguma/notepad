da8a9de39998933604ab51202341a3e9cc574df2
fabrice137 <fabruz3@gmail.com>
2026-01-28 15:25:44 +0200

    added deployment configurations

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Cloud Notepad",
+  title: "Fab Notepad",
   description: "A simple cloud notepad",
   icons: {
     icon: "/notes_favicon.png",
